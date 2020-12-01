f3069e8567d000c4abdb8baeabc5d144df65ef51
James Humphries <jhumphries@vqcomms.com>
2020-12-01 15:56:22 +0000

    overly complicated code to have the same solution for d1p1 and d1p2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,10 +5,10 @@ fn main() {
     let input = read_to_string("input").expect("failed to read input file");
     let expenses = parse(&input).expect("Failed to parse input file");
 
-    let p1 = pt1(&expenses);
+    let p1 = find_result(&expenses, 2).unwrap();
     println!("Part 1 result : {}", p1);
 
-    let p2 = pt2(&expenses);
+    let p2 = find_result(&expenses, 3).unwrap();
     println!("Part 2 result : {}", p2);
 }
 
@@ -16,42 +16,32 @@ fn parse(input: &str) -> Option<Vec<usize>> {
     input.lines().map(|s| s.parse().ok()).collect()
 }
 
-fn pt1<'a>(expenses: &'a Vec<usize>) -> usize {
-    let result = expenses
+fn find_result(expenses: &Vec<usize>, amount_of_numbers: usize) -> Option<usize> {
+    match expenses
         .iter()
-        .tuple_combinations()
-        .find(|&(a, b)| a + b == 2020)
-        .unwrap();
-
-    return result.0 * result.1;
-}
-
-fn pt2<'a>(expenses: &'a Vec<usize>) -> usize {
-    let result = expenses
-        .iter()
-        .tuple_combinations()
-        .find(|&(a, b, c)| a + b + c == 2020)
-        .unwrap();
-
-    return result.0 * result.1 * result.2;
+        .combinations(amount_of_numbers)
+        .find(|c| c.iter().map(|&&x| x).sum::<usize>() == 2020)
+    {
+        Some(combination) => Some(combination.iter().map(|&&x| x).product()),
+        None => None,
+    }
 }
 
 #[cfg(test)]
 mod day1 {
-    use super::pt1;
-    use super::pt2;
+    use super::find_result;
 
     #[test]
     fn part1() {
         let vec = vec![1721, 979, 366, 299, 675, 1456];
-        let result = pt1(&vec);
+        let result = find_result(&vec, 2).unwrap();
         assert_eq!(514579 as usize, result);
     }
 
     #[test]
     fn part2() {
         let vec = vec![1721, 979, 366, 299, 675, 1456];
-        let result = pt2(&vec);
+        let result = find_result(&vec, 3).unwrap();
         assert_eq!(241861950, result);
     }
 }
