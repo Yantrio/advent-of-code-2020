c51543a2f7fe822dfce78eaad43a2eee071f6da6
James Humphries <jhumphries@vqcomms.com>
2020-12-01 10:25:29 +0000

    refactor day 1  to itertools

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,13 +1,14 @@
+use itertools::Itertools;
 use std::fs::read_to_string;
 
 fn main() {
     let input = read_to_string("input").expect("failed to read input file");
     let expenses = parse(&input).expect("Failed to parse input file");
 
-    let p1 = pt1(&expenses).unwrap();
+    let p1 = pt1(&expenses);
     println!("Part 1 result : {}", p1);
 
-    let p2 = pt2(&expenses).unwrap();
+    let p2 = pt2(&expenses);
     println!("Part 2 result : {}", p2);
 }
 
@@ -15,28 +16,24 @@ fn parse(input: &str) -> Option<Vec<usize>> {
     input.lines().map(|s| s.parse().ok()).collect()
 }
 
-fn pt1<'a>(expenses: &'a Vec<usize>) -> Result<usize, &'static str> {
-    for e1 in expenses.iter() {
-        for e2 in expenses.iter() {
-            if e1 + e2 == 2020 {
-                return Ok(e1 * e2);
-            }
-        }
-    }
-    return Err("Failed to find two numbers to add up to 2020");
+fn pt1<'a>(expenses: &'a Vec<usize>) -> usize {
+    let result = expenses
+        .iter()
+        .tuple_combinations()
+        .find(|&(a, b)| a + b == 2020)
+        .unwrap();
+
+    return result.0 * result.1;
 }
 
-fn pt2<'a>(expenses: &'a Vec<usize>) -> Result<usize, &'static str> {
-    for e1 in expenses.iter() {
-        for e2 in expenses.iter() {
-            for e3 in expenses.iter() {
-                if e1 + e2 + e3 == 2020 {
-                    return Ok(e1 * e2 * e3);
-                }
-            }
-        }
-    }
-    return Err("Failed to find three numbers to add up to 2020");
+fn pt2<'a>(expenses: &'a Vec<usize>) -> usize {
+    let result = expenses
+        .iter()
+        .tuple_combinations()
+        .find(|&(a, b, c)| a + b + c == 2020)
+        .unwrap();
+
+    return result.0 * result.1 * result.2;
 }
 
 #[cfg(test)]
@@ -47,14 +44,14 @@ mod day1 {
     #[test]
     fn part1() {
         let vec = vec![1721, 979, 366, 299, 675, 1456];
-        let result = pt1(&vec).unwrap();
+        let result = pt1(&vec);
         assert_eq!(514579 as usize, result);
     }
 
     #[test]
     fn part2() {
         let vec = vec![1721, 979, 366, 299, 675, 1456];
-        let result = pt2(&vec).unwrap();
+        let result = pt2(&vec);
         assert_eq!(241861950, result);
     }
 }
