55a2ee7b0d5b0c4caf82b25580ace4e9cf496f0d
James Humphries <jhumphries@vqcomms.com>
2020-12-02 09:21:47 +0000

    use include_str! rejig class to simplify it

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,13 +1,12 @@
 use lazy_static::lazy_static;
 use regex::Regex;
-use std::fs::read_to_string;
 
 lazy_static! {
     static ref PARSINGREGEX: Regex = Regex::new(r"(\d+)-(\d+) (.): (.+)").unwrap();
 }
 
 fn main() {
-    let input = read_to_string("input").expect("failed to read input file");
+    let input = include_str!("input");
     let passwords = input.lines().map(Password::new).collect::<Vec<Password>>();
 
     let valid_passwords = passwords
@@ -26,41 +25,33 @@ fn main() {
 }
 
 #[derive(Debug)]
-struct Policy {
+struct Password {
+    password: String,
     min: usize,
     max: usize,
     character: char,
-}
-
-#[derive(Debug)]
-struct Password {
-    policy: Policy,
-    password: String,
 }
 
 impl Password {
     fn new(line: &str) -> Self {
         let captures = PARSINGREGEX.captures(line).unwrap();
         Password {
-            policy: Policy {
-                min: captures.get(1).unwrap().as_str().parse().unwrap(),
-                max: captures.get(2).unwrap().as_str().parse().unwrap(),
-                character: captures.get(3).unwrap().as_str().chars().next().unwrap(),
-            },
+            min: captures.get(1).unwrap().as_str().parse().unwrap(),
+            max: captures.get(2).unwrap().as_str().parse().unwrap(),
+            character: captures.get(3).unwrap().as_str().chars().nth(0).unwrap(),
             password: captures.get(4).unwrap().as_str().to_string(),
         }
     }
 
     fn followed_corporate_policy(&self) -> bool {
-        let count = self.password.matches(self.policy.character).count();
-        count >= self.policy.min && count <= self.policy.max
+        let count = self.password.matches(self.character).count();
+        count >= self.min && count <= self.max
     }
 
     fn followed_revised_corporate_policy(&self) -> bool {
-        let a = self.password.chars().nth(self.policy.min - 1).unwrap();
-        let b = self.password.chars().nth(self.policy.max - 1).unwrap();
+        let a = self.password.chars().nth(self.min - 1).unwrap();
+        let b = self.password.chars().nth(self.max - 1).unwrap();
 
-        (a == self.policy.character && b != self.policy.character)
-            || (a != self.policy.character && b == self.policy.character)
+        (a == self.character && b != self.character) || (a != self.character && b == self.character)
     }
 }
