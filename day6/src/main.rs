06cd1d4b8739505da2ac3a764faa975c4cb57713
James Humphries <jhumphries@vqcomms.com>
2020-12-06 10:07:12 +0000

    finished refactoring day 6 after learning a bit more

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,37 +1,34 @@
 use std::collections::HashSet;
 
 fn main() {
-    let input = String::from(include_str!("input"));
+    let groups = || include_str!("input").split("\n\n");
 
-    let p1 = input.split("\n\n").fold(0, |acc, g| {
-        acc + g
-            .chars()
-            .filter(|c| c.is_alphabetic())
-            .collect::<HashSet<char>>()
-            .len()
-    });
-
+    let p1 = groups().fold(0, |acc, g| acc + count_unique_chars(g));
     println!("part 1: {:#?}", p1);
 
-    let p2 = input
-        .split("\n\n")
+    let p2: usize = groups()
         .map(|g| g.lines().collect::<Vec<&str>>())
-        .map(|g| count_all_answered(&g))
-        .sum::<usize>();
-
+        .map(count_all_answered)
+        .sum();
     println!("part 2: {:#?}", p2);
 }
 
-fn count_all_answered(group: &Vec<&str>) -> usize {
-    let alphabet = (b'a'..=b'z').map(char::from);
-    alphabet.fold(0, |acc, c| {
-        acc + all_people_answered_yes_to(group, &c) as usize
-    })
+fn count_unique_chars(input: &str) -> usize {
+    input
+        .chars()
+        .filter(|c| c.is_alphabetic())
+        .collect::<HashSet<_>>()
+        .len()
 }
 
-fn all_people_answered_yes_to(group: &Vec<&str>, c: &char) -> bool {
+fn count_all_answered(group: Vec<&str>) -> usize {
+    let alphabet = (b'a'..=b'z').map(char::from);
+    alphabet.fold(0, |acc, c| acc + all_answered(&group, &c) as usize)
+}
+
+fn all_answered(group: &Vec<&str>, c: &char) -> bool {
     group
         .iter()
-        .map(|g| g.chars().collect::<Vec<char>>())
+        .map(|&g| g.chars().collect::<Vec<char>>())
         .all(|g| g.contains(&c))
 }
