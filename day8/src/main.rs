39beeda8d390c454d63036a375c762bd8479196d
James Humphries <jhumphries@vqcomms.com>
2020-12-08 10:29:33 +0000

    day 8 complete

---
+++ day8/src/main.rs
@@ -0,0 +1,168 @@
+fn main() {
+    let input = include_str!("input");
+    println!("Part 1: {}", pt1(input));
+    println!("Part 2: {}", pt2(input));
+}
+
+fn pt1(input: &str) -> i64 {
+    let c = &mut Computer::new(input);
+
+    let mut seen_ips: Vec<i64> = Vec::new();
+    while !seen_ips.contains(&c.ip) {
+        seen_ips.push(c.ip);
+        c.step();
+    }
+    return c.acc;
+}
+
+fn pt2(input: &str) -> i64 {
+    let reference = &mut Computer::new(input);
+
+    // find all jmps and noops
+    let jmp_or_nop: Vec<(usize, &Instruction)> = reference
+        .instructions
+        .iter()
+        .enumerate()
+        .filter(|(_, instr)| instr.opcode == OpCode::Jmp || instr.opcode == OpCode::Nop)
+        .collect();
+
+    for instr in jmp_or_nop {
+        // construct a computer with it swapped, we could be smarter about this but there's no need
+        let c = &mut Computer::new(include_str!("input"));
+        c.instructions[instr.0].opcode = match c.instructions[instr.0].opcode {
+            OpCode::Nop => OpCode::Jmp,
+            OpCode::Jmp => OpCode::Nop,
+            _ => unreachable!(),
+        };
+        // if it does terminate, return the accumulator value
+        if c.does_terminate() {
+            return c.acc;
+        }
+    }
+    unreachable!()
+}
+
+#[derive(Debug, PartialEq)]
+#[repr(i32)]
+enum OpCode {
+    Acc,
+    Jmp,
+    Nop,
+}
+
+#[derive(Debug, PartialEq)]
+struct Instruction {
+    opcode: OpCode,
+    argument: i64,
+}
+
+#[derive(Debug)]
+struct Computer {
+    pub ip: i64,
+    pub acc: i64,
+    pub instructions: Vec<Instruction>,
+}
+
+impl Computer {
+    fn parse_op_code(input: &str) -> OpCode {
+        match input {
+            "acc" => OpCode::Acc,
+            "jmp" => OpCode::Jmp,
+            "nop" => OpCode::Nop,
+            _ => unreachable!(),
+        }
+    }
+
+    fn new(input: &str) -> Self {
+        Computer {
+            ip: 0,
+            acc: 0,
+            instructions: input
+                .lines()
+                .map(|l| Computer::parse_operation(l))
+                .collect(),
+        }
+    }
+
+    fn reset(&mut self) {
+        self.ip = 0;
+        self.acc = 0;
+    }
+
+    fn print(&self) {
+        println!("IP {}, ACC {}", self.ip, self.acc);
+    }
+
+    fn parse_operation(input: &str) -> Instruction {
+        let op = input.split_ascii_whitespace().collect::<Vec<&str>>();
+        Instruction {
+            opcode: Computer::parse_op_code(op[0]),
+            argument: op[1].parse().unwrap(),
+        }
+    }
+
+    fn step(&mut self) {
+        let curr = &self.instructions[self.ip as usize];
+        match curr.opcode {
+            OpCode::Acc => {
+                self.acc += curr.argument;
+                self.ip += 1;
+            }
+            OpCode::Jmp => self.ip += curr.argument,
+            OpCode::Nop => self.ip += 1,
+        }
+    }
+
+    fn does_terminate(&mut self) -> bool {
+        let mut seen_ips: Vec<i64> = Vec::new();
+        loop {
+            match seen_ips.contains(&self.ip) {
+                true => return false, // we dont have comparisons yet
+                false => match self.ip >= self.instructions.len() as i64 {
+                    true => return true,
+                    false => {
+                        seen_ips.push(self.ip);
+                        self.step();
+                    }
+                },
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod day8 {
+    use super::Computer;
+    use super::Instruction;
+    use super::OpCode;
+
+    fn test_parse(str: &str, expected: Instruction) {
+        let res = Computer::parse_operation(str);
+        assert_eq!(res, expected);
+    }
+
+    #[test]
+    fn should_parse_opcodes() {
+        test_parse(
+            "nop +0",
+            Instruction {
+                opcode: OpCode::Nop,
+                argument: 0,
+            },
+        );
+        test_parse(
+            "acc +1",
+            Instruction {
+                opcode: OpCode::Acc,
+                argument: 1,
+            },
+        );
+        test_parse(
+            "jmp -1",
+            Instruction {
+                opcode: OpCode::Jmp,
+                argument: -1,
+            },
+        );
+    }
+}
