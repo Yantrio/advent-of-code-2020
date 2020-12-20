9f879356cf9ee4e0d1760dd7d15969bf0f4711b4
James Humphries <jhumphries@vqcomms.com>
2020-12-20 23:33:42 +0000

    pt2 done

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::str::FromStr;
 
 fn main() {
@@ -5,9 +6,8 @@ fn main() {
     let tiles = get_tiles(input);
     let (corners, result) = part1(&tiles);
 
-    println!("Corners: {:?}", result);
-
-    part2(&tiles, &corners);
+    println!("Part 1: {:?}", result);
+    println!("Part 2: {:?}", part2(&tiles, &corners));
 }
 
 fn part1(tiles: &Vec<Tile>) -> (Vec<&Tile>, usize) {
@@ -16,16 +16,16 @@ fn part1(tiles: &Vec<Tile>) -> (Vec<&Tile>, usize) {
     for tile in tiles.iter() {
         let (mut n, mut s, mut e, mut w) = (false, false, false, false);
         for comparison in tiles.iter().filter(|t| t.id != tile.id) {
-            if !n && tile.matches_on_north(comparison) {
+            if !n && tile.matches_on_north(comparison).is_some() {
                 n = true;
             }
-            if !s && tile.matches_on_south(comparison) {
+            if !s && tile.matches_on_south(comparison).is_some() {
                 s = true;
             }
-            if !e && tile.matches_on_west(comparison) {
+            if !e && tile.matches_on_west(comparison).is_some() {
                 e = true;
             }
-            if !w && tile.matches_on_east(comparison) {
+            if !w && tile.matches_on_east(comparison).is_some() {
                 w = true;
             }
         }
@@ -44,9 +44,144 @@ fn part1(tiles: &Vec<Tile>) -> (Vec<&Tile>, usize) {
     )
 }
 
-fn part2(tiles: &Vec<Tile>, corners: &Vec<&Tile>) {
-    //solve left edge by getting top left corner first
-    //find top left, then go down!
+fn part2(tiles: &Vec<Tile>, corners: &Vec<&Tile>) -> usize {
+    // solve left edge by getting top left corner first
+    // find top left, then go down!
+    // then go across going down each column in the same way
+
+    let top_left = corners[0];
+
+    let mut poss = vec![];
+    for o in top_left.get_orientations().into_iter() {
+        let match_down = tiles
+            .iter()
+            .filter(|t| t.id != top_left.id)
+            .find(|t| o.matches_on_south(t).is_some());
+
+        let match_right = tiles
+            .iter()
+            .filter(|t| t.id != top_left.id)
+            .find(|t| o.matches_on_east(t).is_some());
+
+        if match_down.is_some() && match_right.is_some() {
+            poss.push(o);
+        }
+    }
+
+    let grid = poss
+        .iter()
+        .map(|p| get_grid(tiles, &p))
+        .find(Result::is_ok)
+        .unwrap()
+        .unwrap();
+
+    find_rough_waters_with_monster(&mut grid_to_tile(&grid))
+}
+
+fn find_rough_waters_with_monster(grid: &mut Tile) -> usize {
+    // we're cheeky and use a tile so we can orient it easier using our methods from pt1
+    let sea_monster = [
+        "                  #",
+        "#    ##    ##    ###",
+        " #  #  #  #  #  #",
+    ];
+
+    let sea_monster_rules: Vec<_> = sea_monster
+        .iter()
+        .enumerate()
+        .flat_map(|(row, &line)| {
+            line.chars()
+                .enumerate()
+                .filter_map(move |(col, c)| match c == '#' {
+                    true => Some((row, col)),
+                    false => None,
+                })
+        })
+        .collect();
+
+    for mut o in grid.get_orientations() {
+        let mut count = 0;
+        for y in 0..(grid.pixels.len() - sea_monster.len()) {
+            for x in 0..(grid.pixels.len() - sea_monster[0].len()) {
+                let found_sea_monster = sea_monster_rules
+                    .iter()
+                    .all(|(dy, dx)| o.pixels[y + dy][x + dx] == '#');
+
+                if found_sea_monster {
+                    for &(dy, dx) in sea_monster_rules.iter() {
+                        o.pixels[y + dy][x + dx] = 'O'
+                    }
+                    count += 1;
+                }
+            }
+        }
+
+        if count > 0 {
+            return o
+                .pixels
+                .iter()
+                .map(|l| l.iter().filter(|&&c| c == '#').count())
+                .sum::<usize>();
+        }
+    }
+    unreachable!();
+}
+
+type Grid = HashMap<(usize, usize), Tile>;
+fn get_grid(tiles: &Vec<Tile>, top_left: &Tile) -> Result<Grid, ()> {
+    let all_tiles_but = |id: usize| tiles.into_iter().filter(move |t| t.id != id);
+
+    let mut grid = HashMap::new();
+    grid.insert((0, 0), top_left.clone());
+
+    let grid_size = (tiles.len() as f64).sqrt() as usize;
+
+    // iterate columns and fill those out too!
+    for x in 0..grid_size {
+        if x != 0 {
+            let up_left = grid.get(&(x - 1, 0)).unwrap();
+            let found = all_tiles_but(up_left.id)
+                .map(|t| up_left.matches_on_east(t))
+                .find(Option::is_some)
+                .unwrap()
+                .unwrap();
+
+            grid.insert((x, 0), found);
+        }
+
+        for y in 1..grid_size {
+            let above = grid.get(&(x, y - 1)).unwrap();
+            let found = all_tiles_but(above.id)
+                .map(|t| above.matches_on_south(t))
+                .find(Option::is_some)
+                .ok_or_else(|| ())?
+                .ok_or_else(|| ())?;
+
+            grid.insert((x, y), found);
+        }
+    }
+
+    Ok(grid)
+}
+
+fn grid_to_tile(grid: &Grid) -> Tile {
+    let tile_size = grid.values().nth(0).unwrap().pixels.len() - 2;
+    let grid_size = (grid.values().len() as f32).sqrt() as usize;
+    let mut lines = (0..(grid_size * tile_size))
+        .map(|_| "".to_string())
+        .collect::<Vec<_>>();
+
+    for y in 0..grid_size {
+        for x in 0..grid_size {
+            let current = grid.get(&(x, y)).unwrap();
+            for (idx, st) in current.to_lines_without_borders().iter().enumerate() {
+                lines[(y * tile_size) + idx] += st;
+            }
+        }
+    }
+    ("Tile 0:\n".to_owned() + &lines.join("\n"))
+        .parse::<Tile>()
+        .unwrap()
 }
 
 fn get_tiles(input: &str) -> Vec<Tile> {
@@ -54,17 +189,6 @@ fn get_tiles(input: &str) -> Vec<Tile> {
         .split("\n\n")
         .map(|t| t.parse::<Tile>().unwrap())
         .collect()
-}
-
-fn intersection<'a>(l: &'a Vec<String>, r: &'a Vec<String>) -> Vec<String> {
-    let mut common = Vec::new();
-    for item in l.iter() {
-        if r.contains(&item) {
-            common.push(item.clone());
-        }
-    }
-
-    common
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -79,10 +203,9 @@ impl FromStr for Tile {
         let mut lines = s.lines();
 
         let id_str = lines.next().unwrap().split("Tile ").nth(1).unwrap();
-        let id = &id_str[0..id_str.len() - 1].parse::<usize>().unwrap();
 
         Ok(Tile {
-            id: *id,
+            id: id_str[0..id_str.len() - 1].parse::<usize>().unwrap(),
             pixels: lines.map(|l| l.chars().collect()).collect(),
         })
     }
@@ -105,10 +228,15 @@ impl Tile {
             self.pixels.iter().map(|p| p[p.len() - 1]).collect(),
         ];
 
-        for i in res.clone().iter() {
-            let reverse = i.chars().rev().collect();
-            res.push(reverse);
-        }
+        // dont forget the mirrored borders
+        res.append(
+            &mut res
+                .clone()
+                .iter()
+                .map(|i| i.chars().rev().collect::<String>())
+                .collect::<Vec<_>>(),
+        );
+
         res
     }
 
@@ -116,22 +244,13 @@ impl Tile {
         let mut current: Tile = self.clone();
         let mut res: Vec<Tile> = Vec::new();
         for _ in 0..4 {
-            for f in current.get_flips() {
-                res.push(f);
-            }
+            res.push(current.clone());
+            res.push(current.clone().flip_x());
+            res.push(current.clone().flip_y());
             current = current.rotate_90();
         }
 
         res.into_iter().map(|r| r).collect()
-    }
-
-    fn get_flips(&self) -> Vec<Tile> {
-        let mut res: Vec<Tile> = Vec::new();
-
-        res.push(self.clone());
-        res.push(self.clone().flip());
-
-        res
     }
 
     fn rotate_90(&self) -> Tile {
@@ -149,222 +268,57 @@ impl Tile {
         res
     }
 
-    fn flip(&self) -> Tile {
+    fn flip_y(&self) -> Tile {
         Tile {
             id: self.id,
             pixels: self.pixels.clone().into_iter().rev().collect(),
         }
     }
 
-    fn matches_on_border(&self, other: &Tile, idx: usize, other_idx: usize) -> bool {
-        let b = self.get_borders();
-        let mut found = false;
-        for orientation in other.get_orientations().iter() {
-            if b[idx] == orientation.get_borders()[other_idx] {
-                found = true;
-                break;
-            }
+    fn flip_x(&self) -> Tile {
+        Tile {
+            id: self.id,
+            pixels: self
+                .pixels
+                .clone()
+                .into_iter()
+                .map(|l| l.iter().rev().map(|&c| c).collect())
+                .collect(),
         }
-        found
     }
 
-    fn matches_on_north(&self, other: &Tile) -> bool {
+    fn matches_on_border(&self, other: &Tile, idx: usize, other_idx: usize) -> Option<Tile> {
+        let b = self.get_borders();
+        for orientation in other.get_orientations().iter() {
+            if b[idx] == orientation.get_borders()[other_idx] {
+                return Some(orientation.clone());
+            }
+        }
+        None
+    }
+
+    fn matches_on_north(&self, other: &Tile) -> Option<Tile> {
         self.matches_on_border(other, 0, 1)
     }
 
-    fn matches_on_south(&self, other: &Tile) -> bool {
+    fn matches_on_south(&self, other: &Tile) -> Option<Tile> {
         self.matches_on_border(other, 1, 0)
     }
 
-    fn matches_on_east(&self, other: &Tile) -> bool {
+    fn matches_on_east(&self, other: &Tile) -> Option<Tile> {
         self.matches_on_border(other, 3, 2)
     }
-    fn matches_on_west(&self, other: &Tile) -> bool {
+
+    fn matches_on_west(&self, other: &Tile) -> Option<Tile> {
         self.matches_on_border(other, 2, 3)
     }
-}
 
-#[cfg(test)]
-mod day20 {
-
-    use super::Tile;
-
-    #[test]
-    fn should_parse_tile() {
-        let input = r#"Tile 3079:
-#.#.#####.
-.#..######
-..#.......
-######....
-####.#..#.
-.#...#.##.
-#.#####.##
-..#.###...
-..#.......
-..#.###..."#;
-
-        let parsed: Tile = input.parse().unwrap();
-
-        assert_eq!(parsed.id, 3079);
-        assert_eq!(parsed.pixels.len(), 10);
-        assert_eq!(parsed.pixels[0].len(), 10);
-        assert_eq!(parsed.pixels[0], "#.#.#####.".chars().collect::<Vec<_>>());
-    }
-
-    #[test]
-    fn should_get_correct_borders() {
-        let input = r#"Tile 3079:
-#.#.#####.
-.#..######
-..#.......
-######....
-####.#..#.
-.#...#.##.
-#.#####.##
-..#.###...
-..#.......
-..#.###..."#;
-        let parsed: Tile = input.parse().unwrap();
-        let borders = parsed.get_borders();
-        let expected = vec!["#.#.#####.", "..#.###...", "#..##.#...", ".#....#..."]
-            .into_iter()
-            .map(|b| {
-                let rev = b.clone().chars().rev().collect::<String>();
-                (b, rev)
-            })
-            .flat_map(|(b, r)| vec![b.chars().collect(), r])
-            .collect::<Vec<String>>();
-        for b in borders {
-            assert_eq!(expected.contains(&b), true);
-        }
-    }
-
-    #[test]
-    fn should_rotate() {
-        let input = r#"Tile 3079:
-#.#.#####.
-.#..######
-..#.......
-######....
-####.#..#.
-.#...#.##.
-#.#####.##
-..#.###...
-..#.......
-..#.###..."#;
-
-        let expected = vec![
-            "...#.##..#",
-            "....###.#.",
-            "####.###.#",
-            "...#.##...",
-            "#.##..#.##",
-            "#.#####.##",
-            "#.##....##",
-            "....#...##",
-            "...###..##",
-            "...#....#.",
-        ]
-        .iter()
-        .map(|l| l.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-
-        let parsed: Tile = input.parse().unwrap();
-        let rotated = parsed.rotate_90();
-
-        assert_eq!(rotated.id, parsed.id);
-        assert_eq!(rotated.pixels, expected);
-    }
-
-    #[test]
-    fn should_flip_y() {
-        let input = r#"Tile 3079:
-#.#.#####.
-.#..######
-..#.......
-######....
-####.#..#.
-.#...#.##.
-#.#####.##
-..#.###...
-..#.......
-..#.###..."#;
-
-        let expected = vec![
-            "..#.###...",
-            "..#.......",
-            "..#.###...",
-            "#.#####.##",
-            ".#...#.##.",
-            "####.#..#.",
-            "######....",
-            "..#.......",
-            ".#..######",
-            "#.#.#####.",
-        ]
-        .iter()
-        .map(|l| l.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-
-        let parsed: Tile = input.parse().unwrap();
-        let flipped = parsed.flip();
-        println!(
-            "{:#?}",
-            flipped
-                .pixels
-                .iter()
-                .map(|p| p.iter().collect::<String>())
-                .collect::<Vec<_>>()
-        );
-
-        assert_eq!(flipped.id, parsed.id);
-        assert_eq!(flipped.pixels, expected);
-    }
-
-    #[test]
-    fn get_orientations() {
-        let input = r#"Tile 3079:
-        #.#.#####.
-        .#..######
-        ..#.......
-        ######....
-        ####.#..#.
-        .#...#.##.
-        #.#####.##
-        ..#.###...
-        ..#.......
-        ..#.###..."#;
-        let parsed: Tile = input.parse().unwrap();
-
-        let orientations = parsed.get_orientations();
-
-        assert_eq!(orientations.len(), 8);
-    }
-
-    #[test]
-    fn matches_on_north() {
-        let a = "Tile 0:\n##\n..".parse::<Tile>().unwrap();
-        let b = "Tile 1:\n..\n##".parse::<Tile>().unwrap();
-        assert_eq!(a.matches_on_north(&b), true);
-    }
-
-    #[test]
-    fn matches_on_south() {
-        let a = "Tile 0:\n##\n..".parse::<Tile>().unwrap();
-        let b = "Tile 1:\n..\n.a".parse::<Tile>().unwrap();
-        assert_eq!(a.matches_on_south(&b), true);
-    }
-    #[test]
-    fn matches_on_east() {
-        let a = "Tile 0:\n##\n..".parse::<Tile>().unwrap();
-        let b = "Tile 1:\n#a\n.a".parse::<Tile>().unwrap();
-        assert_eq!(a.matches_on_east(&b), true);
-    }
-
-    #[test]
-    fn matches_on_west() {
-        let a = "Tile 0:\n#.\n#.".parse::<Tile>().unwrap();
-        let b = "Tile 1:\n.#\n.#".parse::<Tile>().unwrap();
-        assert_eq!(a.matches_on_west(&b), true);
+    fn to_lines_without_borders(&self) -> Vec<String> {
+        self.pixels
+            .iter()
+            .skip(1)
+            .take(self.pixels.len() - 2)
+            .map(|line| line.iter().skip(1).take(line.len() - 2).collect::<String>())
+            .collect::<Vec<_>>()
     }
 }
